5a15e4329229b59b02d7293b2fec79cec43390f8
LunaeKim99 <dyo250307@gmail.com>
2026-08-05 08:13:24 +0700

    fix: Vercel deployment - correct Honoo adapter path

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,8 @@
+import { handle } from 'hono/adapter/vercel'
 import app from '../server/index'
 
-export default app
+export const GET = handle(app)
+export const POST = handle(app)
+export const PUT = handle(app)
+export const DELETE = handle(app)
+export const PATCH = handle(app)
